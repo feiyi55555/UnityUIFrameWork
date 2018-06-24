6ad516c82de0257e9d260616acfd690204ce88c0
wuxudong <457336590@qq.com>
2018-06-24 11:50:44 +0800

    加入Panel上按钮事件的监听听

---
+++ Assets/Scripts/Core/UIBase.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIBase : MonoBehaviour {
+
+    private void Awake()
+    {
+        Button[] btns = GetComponentsInChildren<Button>();
+        for (int i = 0; i < btns.Length; i++)
+        {
+            Button btn = btns[i];
+            EventTriggerListener listener = EventTriggerListener.Get(btn.gameObject);
+            listener.onClick = OnBtnClick;
+        }
+        OnAwake();
+    }
+
+    private void Start ()
+    {
+        OnStart();
+    }
+
+    private void Update () {
+		
+	}
+
+    private void OnDestory()
+    {
+        BeforeOnDestory();
+    }
+
+    protected virtual void OnStart() { }
+    protected virtual void OnAwake() { }
+    protected virtual void BeforeOnDestory() { }
+    protected virtual void OnBtnClick(GameObject go) { }
+}
---
+++ Assets/Scripts/UI/UIPanel/UIPanelLogOnCtrl.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIPanelLogOnCtrl : UIBase
+{
+    protected override void OnBtnClick(GameObject go)
+    {
+        Debug.Log(go.name);
+    }
+}
